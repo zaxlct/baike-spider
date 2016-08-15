b5ceec9d671188c6b98a7309ec2155952dc00df8
zaxlct <zaxlct@foxmail.com>
2016-08-15 23:05:51 +0800

    输出器代码

--- html_outputer.py
+++ html_outputer.py
@@ -1,7 +1,29 @@
 class HtmlOutputer(object):
+    def __init__(self):
+        self.datas = []
 
-    def collect_data(self):
-        pass
+    def collect_data(self, data):
+        if data is None:
+            return
+        self.datas.append(data)
 
     def outputer_html(self)    :
-        pass
+        fout = open('outputer_html', 'w')
+
+        font.write('<html>')
+        font.write('<body>')
+        font.write('<table>')
+
+        # ascii python 默认编码
+        for data in self.datas:
+            fout.write('<tr>')
+            fout.write('<td>%s</td>' % data['url'])
+            fout.write('<td>%s</td>' % data['title'].encode('utf-8'))
+            fout.write('<td>%s</td>' % data['summary'].encode('utf-8'))
+            fout.write('</tr>')
+
+        font.write('</table>')
+        font.write('</body>')
+        font.write('</html>')
+
+        fout.close()
