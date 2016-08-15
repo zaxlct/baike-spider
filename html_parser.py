e7de46513a1c73699ce7149b29ad6cc510266994
zaxlct <zaxlct@foxmail.com>
2016-08-15 22:59:07 +0800

    HTML 解析器逻辑

--- html_parser.py
+++ html_parser.py
@@ -1,4 +1,40 @@
+from bs4 import BeautifulSoup
+import re
+
 class HtmlParser(object):
 
-    def parse(self):
-        pass
+    def _get_new_urls(self, page_url, soup):
+        new_urls = set()
+        # /view/123.html
+        links = soup.find_all('a', href=re.compile(r'/view/\d+\.html'))
+        for link in links:
+            new_url = link['href']
+            # 让 new_url 以 page_url 为模板拼接成一个全新的 url
+            new_full_url = urlparse.urljoin(page_url, new_url)
+            new_urls.add(new_full_url)
+        return new_urls
+
+
+    def _get_new_data(self, page_url, soup):
+        res_data = {}
+
+        # url
+        res_data['url'] = page_url
+
+        # <dd class="lemmaWgt-lemmaTitle-title"> <h1>Python</h1>
+        title_node = soup.find('dd', class_="lemmaWgt-lemmaTitle-title").find("h1")
+        res_data['title'] = title_node.get_text()
+
+        # <div class="lemma-summary">
+        summary_node = soup.find('div', class_="lemma-summary")
+        res_data['summary'] = summay_node.get_text()
+
+        return res_data
+
+    def parse(self, page_url, html_cont):
+        if page_url is None or html_cont is None
+            return
+        soup = BeautifulSoup(html_cont, 'html.parser'， from_encoding = 'utf-8')
+        new_urls = self._get_new_urls(page_url, soup)
+        new_datas = self._get_new_data(page_url, soup)
+        return new_urls, new_data
