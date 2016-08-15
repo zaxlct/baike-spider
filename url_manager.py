f6ab52e39c1a5c51d14600440400fd26b2a02e80
zaxlct <zaxlct@foxmail.com>
2016-08-15 22:24:12 +0800

    url 管理器逻辑

--- url_manager.py
+++ url_manager.py
@@ -1,12 +1,25 @@
 class UrlManager(object):
-    def add_new_url(self):
-        pass
+# url 管理器需要维护待爬取的 url 列表 和 已爬取的 url 列表
+    def __init__(self):
+        self.new_urls = set()
+        self.old_urls = set()
+
+    def add_new_url(self, url):
+        if url is None:
+            return
+        if url not in self.new_urls and url not in self.old_urls:
+            self.new_urls.add(url)
+
+    def add_new_urls(self, urls):
+        if urls is None or len(urls) == 0:
+            return
+        for url in urls:
+            self.add_new_url(url)
 
     def has_new_url(self):
-        pass
+        return len(self.new_urls) != 0
 
     def get_new_url(self):
-        pass
-
-    def add_new_urls(self):
-        pass
+        new_url = self.new_urls.pop()
+        self.old_urls.add(new_url)
+        return new_url
