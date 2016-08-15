d08e7843665c31a79386cd9ad856130fe180c5ef
zaxlct <zaxlct@foxmail.com>
2016-08-15 22:00:16 +0800

    增加异常处理

--- spider_main.py
+++ spider_main.py
@@ -16,21 +16,24 @@ class SpiderMain(object):
 
         # 启动爬虫循环
         white self.urls.has_new_url():
-            # 当 url 管理器里待爬取的 url 时，获取一个 url
-            new_url = self.urls.get_new_url()
-            print('craw %d : %s % (count, new_url)') # 打印传入的第几个 url
-            # 启动下载器并存储
-            html_cont = self.download.download(new_url)
-            # 解析数据
-            new_urls, new_data = self.parser.parse(new_url, html_cont)
-            # 添加进 url 管理器
-            self.urls.add_new_urls(new_urls)
-            # 收集数据
-            self.outputer.collect_data(new_data)
+            try:
+                # 当 url 管理器里待爬取的 url 时，获取一个 url
+                new_url = self.urls.get_new_url()
+                print('craw %d : %s % (count, new_url)') # 打印传入的第几个 url
+                # 启动下载器并存储
+                html_cont = self.download.download(new_url)
+                # 解析数据
+                new_urls, new_data = self.parser.parse(new_url, html_cont)
+                # 添加进 url 管理器
+                self.urls.add_new_urls(new_urls)
+                # 收集数据
+                self.outputer.collect_data(new_data)
 
-            if count == 1000:
-                break
-            count+=1
+                if count == 1000:
+                    break
+                count+=1
+            except:
+                print 'craw failed'
 
         # 输出收集好的数据
         self.outputer.output_html()
